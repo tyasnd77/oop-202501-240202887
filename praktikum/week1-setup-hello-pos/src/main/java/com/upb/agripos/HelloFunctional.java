ed721693e143704db630b71af6fa478e35a41794
TYAS NURSHIKA DAMAIA <tyasnd07@gmail.com>
2025-10-09 11:41:28 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloFunctional.java
@@ -0,0 +1,9 @@
+        // HelloFunctional.java
+public class HelloFunctional {
+   public static void main(String[] args) {
+      String nim = "240202887";
+      String nama = "Tyas"; 
+      System.out.println("Hello World , I am"+ " " + nama + "-" + nim);
+      
+   }
+}
