7e1714ee0d66f6cb37bd178f66553b8801f0e5cf
TYAS NURSHIKA DAMAIA <tyasnd07@gmail.com>
2025-10-10 09:20:14 +0700

    Delete praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java

--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
+++
@@ -1,9 +0,0 @@
-// HelloProcedural.java
-public class HelloProcedural {
-     public static void main(String[] args) {
-      String nim = "240202887";
-      String nama = "Tyas";
-      
-      System.out.println("Hello World, I am" + " " + nama + "-" + nim);
-   }
-}
